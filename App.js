035252c21f05a24ec386bf6d446af8cbc0f18f98
Taylor Furman <TFurman47@gmail.com>
2022-10-15 11:22:58 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HelloWorld } from './src/RunningRoyale';
+
+const App = () => {
+
+  return (
+    <HelloWorld/>
+  );
+};
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+export function HelloWorld(params) {
+
+    const text = "Hello World"
+
+    return(
+        <Text>{text}</Text>
+    )
+    
+} 
